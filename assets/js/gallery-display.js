3d410f89c17c512693bbb9cc7ddafe797c6764a7
xiongyichun <yichunxiong_bio@126.com>
2023-08-24 18:44:59 +0800

    Update gallery-display.js

--- assets/js/gallery-display.js
+++ assets/js/gallery-display.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
 	
 	//lightGallery($(".photoGallery"));
 	//lightGallery(document.getElementsByClassName('photoGallery'));
-	$( '.photoGallery' ).lightGallery()
+	//$( '.photoGallery' ).lightGallery();
+	var lg = document.querySelectorAll( '.photoGallery');
+	for ( var i = 0; i < lg.length; i++ ) {
+		lightGallery( lg[i]);
+	}
 });
